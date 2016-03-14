f18d8280700e8f7f4e2f8d7e60baee1088e376ed
Israel Torres <itorres1490@gmail.com>
2016-03-14 00:48:48 -0700

    Removed temp code and added comments for todo's

--- cert.py
+++ cert.py
@@ -2,7 +2,7 @@ from subprocess import Popen, PIPE
 import array
 
 # input file
-#inputFile = open('wiki_wild_card.pem', 'r')
+# need to add command line options
 inputFile = open('certificates.pem', 'r')
 
 # certificate tags
@@ -12,40 +12,26 @@ endingTag = '-----END CERTIFICATE-----'
 # certificates
 certificates = []
 
+# new certificate in file
 publicCert = ''
 
+# read file line by line
 for line in inputFile:
+	# top of cert
 	if (str.strip(line) == beginningTag):
 		publicCert += beginningTag + '\n'
+	# end of cert, append to certs array and clear contents if we find a new cert
 	elif (str.strip(line) == endingTag):
 		publicCert += endingTag
 		certificates.append(str(publicCert))
 		publicCert = ''
+	# hopefully just an encoded line
 	else:
 		publicCert += line
 
-
 for certficate in certificates:
-	#tempCertFile = open('tempCertFile.pem', 'w')
-	#tempCertFile.write(str(certficate))
-	#tempCertFile.close()
-
-	#tempCertFile = open('tempCertFile.pem', 'r')
-
+	# Currently printing everything, need to add more filters/options, and identify chains
 	cmdCall = Popen(['openssl', 'x509', '-noout', '-text'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 	stdout, stderr = cmdCall.communicate(certficate)
 
 	print stdout
-
-	# tempCertFile.close()
-
-	# print stdout
-	# print stderr
-
-
-# Print contents
-# date = subprocess.check_output(['date'])
-
-# Write to file
-# derFile = file('wiki_wikd_card.cer', 'w')
-# derFile.write(derContents)
