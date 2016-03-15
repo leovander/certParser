4ba4baa70fd83d264befa1e9428a2fc9becec0c0
Israel Torres <itorres1490@gmail.com>
2016-03-14 23:56:47 -0700

    Update cert.py
    Using Jonf's suggestion to use with when opening a file. It looks like you still need to close the file though.

--- cert.py
+++ cert.py
@@ -1,10 +1,6 @@
 from subprocess import Popen, PIPE
 import array
 
-# input file
-# need to add command line options
-inputFile = open('certificates.pem', 'r')
-
 # certificate tags
 beginningTag = '-----BEGIN CERTIFICATE-----'
 endingTag = '-----END CERTIFICATE-----'
@@ -15,19 +11,23 @@ certificates = []
 # new certificate in file
 publicCert = ''
 
-# read file line by line
-for line in inputFile:
-	# top of cert
-	if (str.strip(line) == beginningTag):
-		publicCert += beginningTag + '\n'
-	# end of cert, append to certs array and clear contents if we find a new cert
-	elif (str.strip(line) == endingTag):
-		publicCert += endingTag
-		certificates.append(str(publicCert))
-		publicCert = ''
-	# hopefully just an encoded line
-	else:
-		publicCert += line
+# input file
+# need to add command line options
+with open('certificates.pem', 'r') as inputFile:
+	# read file line by line
+	for line in inputFile:
+		# top of cert
+		if (str.strip(line) == beginningTag):
+			publicCert += beginningTag + '\n'
+		# end of cert, append to certs array and clear contents if we find a new cert
+		elif (str.strip(line) == endingTag):
+			publicCert += endingTag
+			certificates.append(str(publicCert))
+			publicCert = ''
+		# hopefully just an encoded line
+		else:
+			publicCert += line
+inputFile.close()
 
 for certficate in certificates:
 	# Currently printing everything, need to add more filters/options, and identify chains
