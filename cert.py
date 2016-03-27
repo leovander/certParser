a23304822cff51125224dd625d4a647296800b01
Israel Torres <itorres1490@gmail.com>
2016-03-27 15:31:43 -0700

    - Using Anaconda linter - Including sample certs in the repo

--- cert.py
+++ cert.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python
 from subprocess import Popen, PIPE
-import array
 
 # input file
 # need to add command line options
@@ -17,21 +17,25 @@ publicCert = ''
 
 # read file line by line
 for line in inputFile:
-	# top of cert
-	if (str.strip(line) == beginningTag):
-		publicCert += beginningTag + '\n'
-	# end of cert, append to certs array and clear contents if we find a new cert
-	elif (str.strip(line) == endingTag):
-		publicCert += endingTag
-		certificates.append(str(publicCert))
-		publicCert = ''
-	# hopefully just an encoded line
-	else:
-		publicCert += line
+    # top of cert
+    if (str.strip(line) == beginningTag):
+        publicCert += beginningTag + '\n'
+    # end of cert, append to certs array and clear contents if we find a new
+    # cert
+    elif (str.strip(line) == endingTag):
+        publicCert += endingTag
+        certificates.append(str(publicCert))
+        publicCert = ''
+    # hopefully just an encoded line
+    else:
+        publicCert += line
 
 for certficate in certificates:
-	# Currently printing everything, need to add more filters/options, and identify chains
-	cmdCall = Popen(['openssl', 'x509', '-noout', '-text'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-	stdout, stderr = cmdCall.communicate(certficate)
+    # Currently printing everything, need to add more filters/options, and
+    # identify chains
+    cmdCall = Popen(
+        ['openssl', 'x509', '-noout', '-text'],
+        stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = cmdCall.communicate(certficate)
 
-	print stdout
+    print stdout
